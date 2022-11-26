c2c88ad257a4a8be0934a98f2a5c6a582943a847
karadenizemirr <karadenizemirr@gmail.com>
2022-11-26 18:03:26 +0300

    modified:   test.py

--- test.py
+++ test.py
@@ -2,9 +2,13 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
 
 options = Options()
+options.add_argument("start-maximized")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
 
 driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 driver.get("https://www.akakce.com/")
-time.sleep(60)
+time.sleep(3600)
