52614c69db151c771bf32410e71240d65979a886
karadenizemirr <karadenizemirr@gmail.com>
2022-11-26 14:44:52 +0300

    modified:   test.py

--- test.py
+++ test.py
@@ -5,4 +5,10 @@ URL = "https://www.akakce.com"
 session = requests.Session()
 
 req = session.get(URL)
-print(req.text)
+html = BeautifulSoup(req.text, "html.parser")
+
+md = html.findAll("input", {"name": "md"})
+r = html.findAll("input", {"name": "r"})
+
+
+print(r)
