de4c6aae731cbe15991a4dd1e68211b361db4665
karadenizemirr <karadenizemirr@gmail.com>
2022-11-26 16:01:56 +0300

    modified:   test.py

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
 s = requests.Session()
-req = s.get("http://api.akakce.com/", verify=False)
+req = s.get("http://api.akakce.com/")
 print(req.text)
