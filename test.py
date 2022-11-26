2854b2d393e26edddead99b6db16a512a86e0898
karadenizemirr <karadenizemirr@gmail.com>
2022-11-26 15:45:30 +0300

    modified:   test.py

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ session = requests.Session()
 
 URL = "https://api.akakce.com/"
 
-print(session.get(URL, headers = {
-    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
-}, allow_redirects=True).text)
+req = session.get(URL)
+
+print(req.text)
