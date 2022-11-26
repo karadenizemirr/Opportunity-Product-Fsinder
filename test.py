f5ea45c483a0067354dd84cc1f913fc717ade282
karadenizemirr <karadenizemirr@gmail.com>
2022-11-26 13:17:24 +0300

    modified:   test.py

--- test.py
+++ test.py
@@ -6,10 +6,4 @@ session = requests.Session()
 base_url ="https://www.akakce.com"
 
 s_req = session.get(base_url)
-html = BeautifulSoup(s_req.text, 'html.parser')
-
-md = html.findAll('input', {'name': 'md'})
-r = html.findAll('input', {'name', 'r'})
-
-print(md)
-print(r)
+html = BeautifulSoup(s_req.text, 'html.parser').findAll("form")
