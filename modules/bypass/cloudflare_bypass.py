533d56104313e68349fca349b96d9643e47f2bcd
karadenizemirr <karadenizemirr@gmail.com>
2022-11-26 15:54:42 +0300

    modified:   modules/bypass/cloudflare_bypass.py

--- modules/bypass/cloudflare_bypass.py
+++ modules/bypass/cloudflare_bypass.py
@@ -3,8 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-service = webdriver.Chrome(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+options = Options()
+
+options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_argument("--disable-extensions")
+options.add_experimental_option('useAutomationExtension', False)
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
 
 def create_bypass(URL):
     driver.get(URL)
