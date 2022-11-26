759ff593fb67bda52ecbc88afa7c5a6ccd3b4ec7
karadenizemirr <karadenizemirr@gmail.com>
2022-11-26 14:55:37 +0300

    modified:   test.py

--- test.py
+++ test.py
@@ -8,5 +8,11 @@ req = session.get(URL)
 html = BeautifulSoup(req.text, "html.parser")
 
 action = html.findAll("form")[0]['action']
-md = html.findAll("input", {'name': 'md'})
-print(md)
+md = html.findAll("input", {'name': 'md'})[0]["value"]
+r = html.findAll("input", {'name': 'r'})[0]['value']
+
+r_req = session.post(f"{URL}{action}", params={
+    "md": md,
+    "r": r
+})
+print(r_req)
