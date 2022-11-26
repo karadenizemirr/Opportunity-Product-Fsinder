578c1fd072c46c2c78ff86c6c6d9d062ccc70947
karadenizemirr <karadenizemirr@gmail.com>
2022-11-26 18:37:50 +0300

    modified:   modules/create_source.py 	modified:   test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 from modules import create_source
 
-create_source.source(URL="https://api.akakce.com/son-alti-ayin-en-ucuz-fiyatli-urunleri/")
+print(create_source.source(URL="https://api.akakce.com/son-alti-ayin-en-ucuz-fiyatli-urunleri/"))
