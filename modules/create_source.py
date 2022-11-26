047060ebbeacf6e152c96b264ba02627d6307235
karadenizemirr <karadenizemirr@gmail.com>
2022-11-26 22:39:43 +0300

    modified:   modules/create_source.py

--- modules/create_source.py
+++ modules/create_source.py
@@ -12,8 +12,8 @@ options = Options()
 options.add_argument("start-maximized")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
-#options.add_argument('--headless')
-#options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 
 def source(URL=None, proxy = None, user_agent=None):
@@ -23,5 +23,6 @@ def source(URL=None, proxy = None, user_agent=None):
     if re.findall(r'403 Forbidden', str(driver.page_source)):
         driver.delete_all_cookies()
         time.sleep(5)
-
+    
+    driver.close()
     return driver.page_source
