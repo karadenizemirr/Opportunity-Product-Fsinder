113b72e4a74d19b705b6f79e07611f100e6c8e78
karadenizemirr <karadenizemirr@gmail.com>
2022-11-26 16:09:21 +0300

    modified:   test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
-'referer' :"http://api.akakce.com/"}
+'referer' :"http://akakce.com/"}
 
 s = requests.Session()
-req = s.get("http://api.akakce.com/")
+req = s.get("http://akakce.com/")
 print(req.text)
