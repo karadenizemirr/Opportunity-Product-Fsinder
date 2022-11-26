d5df8e8aeaf5a338a505526e3c8d4020d3d06d99
karadenizemirr <karadenizemirr@gmail.com>
2022-11-26 15:12:56 +0300

    modified:   test.py

--- test.py
+++ test.py
@@ -3,4 +3,6 @@ session = requests.Session()
 
 URL = "https://api.akakce.com/"
 
-print(session.get(URL).text)
+print(session.get(URL, headers = {
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+}).text)
