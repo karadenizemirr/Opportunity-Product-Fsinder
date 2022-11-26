fc2c1fc54374c0e624ccc7df0fd4411978b3865b
karadenizemirr <karadenizemirr@gmail.com>
2022-11-26 13:15:52 +0300

    modified:   test.py

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
 
 session = requests.Session()
 
 base_url ="https://www.akakce.com"
 
 s_req = session.get(base_url)
-print(s_req.text)
+html = BeautifulSoup(s_req.text, 'html.parser')
+
+md = html.findAll('input', {'name': 'md'})
+r = html.findAll('input', {'name', 'r'})
+
+print(md)
+print(r)
