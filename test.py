29ce5d47d5e812aee6ce8c5fe7a885355bab7e5d
karadenizemirr <karadenizemirr@gmail.com>
2022-11-26 18:35:07 +0300

    modified:   test.py

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-from modules import create_source
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-create_source.source()
+options = Options()
+options.add_argument("start-maximized")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+#options.add_argument('--headless')
+#options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+driver.get("https://api.akakce.com/son-alti-ayin-en-ucuz-fiyatli-urunleri/")
+time.sleep(3600)
