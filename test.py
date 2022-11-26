b15e0bb3ac37bda39a2fa2749e6e87ae8f2615a2
karadenizemirr <karadenizemirr@gmail.com>
2022-11-26 16:01:22 +0300

    modified:   test.py

--- test.py
+++ test.py
@@ -1,21 +1,6 @@
 import requests
-headers = {
-    "Host": "api.akakce.com",
-    "Sec-Ch-Ua": '"Chromium";v="107", "Not=A?Brand";v="24"',
-    "Sec-Ch-Ua-Mobile": "?0",
-    "Sec-Ch-Ua-Platform": "Linux",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.107 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "Sec-Fetch-Site": "none",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-User": "?1",
-    "Sec-Fetch-Dest": "document",
-    "Accept-Encoding": "gzip, deflate",
-    "Accept-Language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Connection": "close"
-}
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
 s = requests.Session()
-req = s.get("https://api.akakce.com/", verify=False)
+req = s.get("http://api.akakce.com/", verify=False)
 print(req.text)
