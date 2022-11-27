c48b629e68bf76e575117f59c70b064e9a50682d
karadenizemirr <karadenizemirr@gmail.com>
2022-11-27 14:31:28 +0300

    modified:   test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import cloudscraper
 
-cs = cloudscraper.create_scraper()
-req = cs.get("https://api.akakce.com")
-print(req)
+cs = cloudscraper.create_scraper(delay=10)
+while True:
+    req = cs.get("https://api.akakce.com")
+    print(req)
